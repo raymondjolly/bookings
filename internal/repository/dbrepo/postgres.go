564401981d73d8c13c7a84a20af94dde17335cc7
Raymond Jolly <raymondjolly@Raymonds-MacBook-Pro.local>
2022-09-10 17:09:24 -0400

    added a migration to remove a null restriction for the reservation_id in the restrictions table. Added SearchAvailabilityForAllRooms method to the repository package

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -43,7 +43,7 @@ func (m *postgresDBRepo) InsertRoomRestriction(res models.RoomRestriction) error
 }
 
 // SearchAvailabilityByDates returns true if an availablity exists and false if no availability exits
-func (m *postgresDBRepo) SearchAvailabilityByDates(startDate, endDate time.Time, roomId int) (bool, error) {
+func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(startDate, endDate time.Time, roomId int) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
@@ -59,4 +59,37 @@ func (m *postgresDBRepo) SearchAvailabilityByDates(startDate, endDate time.Time,
 		return true, nil
 	}
 	return false, nil
+}
+
+// SearchAvailabilityForAllRooms returns a slice of available rooms, if any for a given date range
+func (m *postgresDBRepo) SearchAvailabilityForAllRooms(startDate, endDate time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	var rooms []models.Room
+
+	query := `
+		select r.id, r.room_name
+		from rooms r
+		where r.id not in (select rr.room_id from room_restrictions rr where $1 <rr.end_date and $2 >rr.start_date)`
+
+	rows, err := m.DB.QueryContext(ctx, query, startDate, endDate)
+	if err != nil {
+		return rooms, err
+	}
+	for rows.Next() {
+		var room models.Room
+		err := rows.Scan(
+			&room.ID,
+			&room.RoomName,
+		)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+	return rooms, nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,5 +9,6 @@ type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
-	SearchAvailabilityByDates(startDate, endDate time.Time, roomId int) (bool, error)
+	SearchAvailabilityByDatesByRoomID(startDate, endDate time.Time, roomId int) (bool, error)
+	SearchAvailabilityForAllRooms(startDate, endDate time.Time) ([]models.Room, error)
 }
