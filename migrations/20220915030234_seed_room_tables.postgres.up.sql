fb79a46f866a9b76c9b06b7f6072762d27a92871
Raymond Jolly <raymond.jolly@gmail.com>
2022-09-25 17:02:28 -0400

    corrected the seed room migration and created a new migration to add a processed field for the reservations table

--- migrations/20220915030234_seed_room_tables.postgres.up.sql
+++ migrations/20220915030234_seed_room_tables.postgres.up.sql
@@ -1,4 +1,3 @@
-INSERT INTO public.rooms(room_name, created_at, updated_at) VALUES
-
-("General's Quarters",'2022-09-10 11:56:36.000000','2022-09-10 11:56:38.000000'),
-("Colonel's Suite",'2022-09-10 11:56:57.000000','2022-09-10 11:56:59.000000');
+INSERT INTO bookings.public.rooms(room_name, created_at, updated_at) VALUES
+('General''s Quarters','2022-09-10','2022-09-10'),
+('Colonel''s Suite','2022-09-10','2022-09-10');
